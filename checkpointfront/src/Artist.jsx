23a8c11db4a6497e81ae3245c6e0432afe117493
Mathias T <mathiastujague@gmail.com>
2019-07-16 18:49:53 +0200

    formulaires en dur

--- checkpointfront/src/Artist.jsx
+++ checkpointfront/src/Artist.jsx
@@ -6,7 +6,10 @@ class Artist extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      artists: [{name: 'Gogo', logo: 'img1', presentation: 'Gogo est un clown joyeux et triste à la fois', discipline: 'clown'},
+      {name: 'Yasa', logo: 'img2', presentation: 'Spectaculaire dressage de fauves', discipline: 'dressage'},
+      {name: 'BingBong', logo: 'img3', presentation: 'Les balles rebondissent à n\'en plus finir.', discipline: 'jonglerie'}
+    ],
     }
   }
   render(){
--- checkpointfront/src/Event.jsx
+++ checkpointfront/src/Event.jsx
@@ -6,6 +6,7 @@ class Event extends Component {
   constructor(props){
     super(props);
     this.state = {
+      
 
     }
   }
