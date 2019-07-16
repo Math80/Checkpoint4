eb7d50b10e1bb14245cdbbb9cac585bc4652137f
Mathias T <mathiastujague@gmail.com>
2019-07-16 11:53:50 +0200

    config front et back

---
+++ checkpointfront/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import allReducers from './reducers';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const store = createStore(
+  allReducers,
+
+  );
+
+  ReactDOM.render(
+    <Provider store ={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
