36f7f50bbd56d33c0b46ae902e4ca61631e989bc
Mathias T <mathiastujague@gmail.com>
2019-07-16 15:18:49 +0200

    footer et css navbar et footer

--- checkpointfront/src/App.js
+++ checkpointfront/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
 import NavBar from './NavBar';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <p>TEST</p>
+      <Footer />
     </div>
   );
 }
