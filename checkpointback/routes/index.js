acbc9ea6077f65ac76a910d16737c29ba7112cc5
Mathias T <mathiastujague@gmail.com>
2019-07-17 18:22:21 +0200

    modif routes

--- checkpointback/routes/index.js
+++ checkpointback/routes/index.js
@@ -74,7 +74,6 @@ router.post('/newevent', (req, response) => {
   const formData = req.body;
   connection.query('INSERT INTO event SET ?', formData, (err) => {
     if (err) {
-      console.log(err);
       response.sendStatus(500);
     } else {
       response.sendStatus(200);
