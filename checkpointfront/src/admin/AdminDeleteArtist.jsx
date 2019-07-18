a3095e12e26f49ab94f9dbc91850f9044556542f
Mathias T <mathiastujague@gmail.com>
2019-07-18 10:25:34 +0200

    composant delete artist fonctionnel

--- checkpointfront/src/admin/AdminDeleteArtist.jsx
+++ checkpointfront/src/admin/AdminDeleteArtist.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 import './AdminDeleteArtist.scss';
 
 
@@ -9,7 +10,7 @@ class AdminDeleteArtist extends Component {
       artists: []
     }
   }
-
+  
   componentDidMount(){
     fetch('http://localhost:3000/api/artists/name')
       .then(res => res.json())
@@ -20,22 +21,42 @@ class AdminDeleteArtist extends Component {
       });
   }
 
+  onDelete = (event) => {
+    const id = event.target.value;
+    console.log(id);
+    const config = {
+      method: 'DELETE',
+    };
+    const url = `http://localhost:3000/api/artist/off/${id}`;
+    fetch(url, config)
+    .then((res) => {
+      if (res.ok) {
+        NotificationManager.success('', 'Artiste supprimé avec succès!');
+      } else {
+        NotificationManager.warning('', 'Erreur lors de suppression de l\'artiste.', 3000);
+      }
+    }).catch(() => {
+      NotificationManager.error('', 'Erreur lors de suppression de l\'artiste.', 5000);
+    });
+  }
+
   render(){
     const { artists } = this.state;
     return(
       <div className="AdminDeleteArtist">
         <div className="title">
-          <h1>ARTISTES</h1>
+          <h1>SUPPRESSSION ARTISTES</h1>
         </div>
         <div className="ArtistList">
           <ul>
             { artists.map(item => (
               <li key={item.id}>
-                {item.name} {item.discipline} <button type="button">Supprimer</button>
+                {item.name} {item.discipline} <button type="button" value={item.id} onClick={ this.onDelete }>Supprimer</button>
               </li>
             ))}
           </ul> 
         </div>
+        <NotificationContainer />
       </div>
     )
   }
